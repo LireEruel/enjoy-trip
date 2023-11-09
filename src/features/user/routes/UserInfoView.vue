928f9be8580f20aa63d5d42fe9bc2d26ffebc7dd
= <=>
2023-11-09 11:14:37 +0900

    마이페이지 header 추가

--- src/features/user/routes/UserInfoView.vue
+++ src/features/user/routes/UserInfoView.vue
@@ -5,10 +5,20 @@
         <h1>계정</h1>
         <a>로그아웃</a>
       </nav>
-      <p>서희 님, 안녕하세요.</p>
-      <div>
-        <a> 개인정보 수정</a>
-        <a> 애인 조회</a>
+      <div class="header-content">
+        <p class="greeting">
+          서희 님, 안녕하세요.
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Blue%20Heart.png"
+            alt="Blue Heart"
+            width="70"
+            height="70"
+          />
+        </p>
+        <div class="header-feature-wrap">
+          <a> 개인정보 수정</a>
+          <a> 애인 조회</a>
+        </div>
       </div>
     </header>
   </div>
@@ -17,7 +27,7 @@
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-@mixin more-button {
+@mixin more-button($font-size: number) {
   background: transparent;
   border: 0;
   color: #06c;
@@ -28,6 +38,7 @@
   padding: 0;
   vertical-align: inherit;
   cursor: pointer;
+  font-size: $font-size;
   &::after {
     content: ">";
     padding-left: 0.3em;
@@ -35,19 +46,35 @@
   }
 }
 header {
-  background-color: #e6f4ff;
+  background-color: #e6f7ff;
+  padding: 0 200px;
   nav {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1rem 2rem;
+    padding: 1rem 0;
+    border-bottom: 1px solid #91d5ff;
     h1 {
       font-size: 1.2rem;
-      font-weight: 700;
+      font-weight: 600;
     }
     a {
-      @include more-button;
-      font-size: 0.8em;
+      @include more-button(0.7em);
+    }
+  }
+  .header-content {
+    padding: 2rem 1rem 2rem;
+    .greeting {
+      font-size: 1.8rem;
+      font-weight: 700;
+      margin-bottom: 1rem;
+    }
+    .header-feature-wrap {
+      display: flex;
+      gap: 1em;
+      a {
+        @include more-button(0.9em);
+      }
     }
   }
 }
