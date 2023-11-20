b840778c74a7e31aa3b8ab8a22d4929c01addc91
= <=>
2023-11-20 15:32:07 +0900

    router 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import { protectedRoutes } from "./protected";
 import { publicRoutes } from "./public";
 
-const setting: any = localStorage.getItem("user");
+// const routes =
+//   setting.user && setting.user.userInfo
+//     ? [...protectedRoutes, ...publicRoutes]
+//     : publicRoutes;
 
-const routes =
-  setting.user && setting.user.userInfo
-    ? [...protectedRoutes, ...publicRoutes]
-    : publicRoutes;
+const routes = [...protectedRoutes, ...publicRoutes];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
