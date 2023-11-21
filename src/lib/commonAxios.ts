42e180807a9c521e044d6d2d110cf94aeef8e96c
LireEruel <jsh001505@gmail.com>
2023-11-21 20:46:30 +0900

    403 에러 처리

--- src/lib/commonAxios.ts
+++ src/lib/commonAxios.ts
@@ -1,9 +1,10 @@
 import { useUserStore } from "@/stores/user";
 import { notification } from "ant-design-vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
 const cancelTokenSource = axios.CancelToken.source();
-
+const router = useRouter();
 const commonAxios = axios.create({
   cancelToken: cancelTokenSource.token,
   baseURL: "http://219.255.6.129:23333",
@@ -32,17 +33,15 @@ commonAxios.interceptors.response.use(
     console.error("error", error?.response);
     const errorCode = String(error?.response?.status ?? "");
 
-    if (errorCode === "401") {
+    if (errorCode === "403") {
       notification.error({
         key: "error-notification",
         message: "토큰이 만료되었습니다. 로그인을 다시 해주세요.",
         description: errorCode,
       });
-    } else if (errorCode === "403") {
-      notification.error({
-        key: "error-notification",
-        message: "권한이 없습니다.",
-        description: errorCode,
+      localStorage.clear();
+      router.push({
+        name: "home",
       });
     } else {
       notification.error({
